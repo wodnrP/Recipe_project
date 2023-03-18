924bb5911eeb19b7a40716f3fc0a6e3fe15d2341
박재욱 <parkjaewook@bagjaeug-ui-Macmini.local>
2023-03-18 17:57:21 +0900

    s3 image server create, dummy data add

--- config/settings.py
+++ config/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'storage.apps.StorageConfig',
     'user.apps.UserConfig',
     'rest_framework',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -148,3 +149,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+###########################AWS
+AWS_ACCESS_KEY_ID = my_settings.AWS_ACCESS_KEY_ID 
+AWS_SECRET_ACCESS_KEY = my_settings.AWS_SECRET_ACCESS_KEY 
+AWS_REGION = 'ap-northeast-2'
+
+###S3 Storages
+AWS_STORAGE_BUCKET_NAME = 'recipedict' # 설정한 버킷 이름
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME,AWS_REGION)
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'path/to/store/my/files/')
