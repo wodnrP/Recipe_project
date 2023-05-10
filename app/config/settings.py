11bb54de52f6ac6962636ab0d36998cb9ff918fd
박재욱 <parkjaewook@bagjaeug-ui-Macmini.local>
2023-05-10 14:56:44 +0900

    settings 변경

--- app/config/settings.py
+++ app/config/settings.py
@@ -37,9 +37,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
-CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000"]
+CSRF_TRUSTED_ORIGINS = env('CSRF_TRUSTED_ORIGINS')
 
 AUTH_USER_MODEL = 'user.User'
 # Application definition
